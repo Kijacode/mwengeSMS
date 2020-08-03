ebb58f5aeca37c6c2f57faf35fe3016e14484da0
Kijacode <kijadanford@gmail.com>
2020-08-03 16:58:29 +0300

    change embedded url to route

--- routes/test/test.js
+++ routes/test/test.js
@@ -7,16 +7,17 @@ const BongLive_2 =  cendSms.bongoSendSms;
 
 
 
-router.get("/send/mwenge/:number/:message",async (req,res) =>{
+router.get("/send/mwenge/:number/:message/:url",async (req,res) =>{
    let phoneNumber = req.params.number;
    let message = req.params.message;
+   let url = req.params.url;
 
    try {
       console.log("start");
       await BongLive_2(phoneNumber,message).then(() =>{
          if(res.statusCode == 200)
          {
-          return  res.redirect("https://uqweli.website/")
+          return  res.redirect(url)
          }
          else{
             return console.log("status code is not okay")
